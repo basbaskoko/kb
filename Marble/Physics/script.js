6f3c5d73c862cb20ece106600d0acc0ced569b52
basbaskoko <56895401+basbaskoko@users.noreply.github.com>
2021-08-22 23:14:53 -0500

    Add files via upload

---
+++ Marble/Physics/script.js
@@ -0,0 +1,19 @@
+document.getElementById("VectorIntro").addEventListener('ended', function(){
+    document.getElementById("VectorIntro").load();
+});
+
+document.getElementById("VectorAddition").addEventListener('ended', function(){
+    document.getElementById("VectorAddition").load();
+});
+
+document.getElementById("VectorMultiplication").addEventListener('ended', function(){
+    document.getElementById("VectorMultiplication").load();
+});
+
+document.getElementById("KinematicsIntroduction").addEventListener('ended', function(){
+    document.getElementById("KinematicsIntroduction").load();
+});
+
+document.getElementById("ProjectileMotion").addEventListener('ended', function() {
+    document.getElementById("ProjectileMotion").load();
+});
